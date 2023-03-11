d324a17c142be5e54c7e5f557bbf482bea9666ea
Bestaford <butenko1demitr@gmail.com>
2023-03-11 11:55:35 +0300

    Initial commit

---
+++ src/Bestaford/Schematics/Schematics.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bestaford\Schematics;
+
+use pocketmine\plugin\PluginBase;
+
+class Schematics extends PluginBase {
+
+    protected function onEnable(): void {
+        $this->getLogger()->info("Started");
+    }
+}
